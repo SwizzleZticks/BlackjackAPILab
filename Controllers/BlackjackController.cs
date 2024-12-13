7d2e4741db4ee67d685b9accf393102fbac241bf
Joshua DiChello <48339347+SwizzleZticks@users.noreply.github.com>
2024-12-13 11:31:31 -0500

    initial commit

---
+++ Controllers/BlackjackController.cs
@@ -0,0 +1,50 @@
+﻿using BlackJackLab.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlackJackLab.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlackjackController : ControllerBase
+    {
+        // This holds the current game state
+        private static GameState? _gameState;
+        private static HttpClient _httpClient = new HttpClient();
+
+        private const string BASE_URL = "https://deckofcardsapi.com/api/deck";
+
+        public BlackjackController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult GetGame()
+        {
+            if (_gameState != null)
+            {
+                return Ok(_gameState);
+            }
+            else
+            {
+                return NotFound("No game started.");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> NewGame()
+        {
+            // TODO create new GameState instance, set starting values,
+            // and set _gameState.
+            return Created("/api/Blackjack", _gameState);
+        }
+
+        [HttpPost("play")]
+        public async Task<IActionResult> Play(string action)
+        {
+            // TODO handle each action.
+            // Else, respond "Invalid action"
+            return ValidationProblem($"Invalid action: {action}.");
+        }
+    }
+}
---
+++ Models/GameState.cs
@@ -0,0 +1,20 @@
+﻿namespace BlackJackLab.Models
+{
+    public class GameState
+    {
+        public string DeckId { get; }
+        // The type "Card" will be part of the model you generate for the
+        // Deck of Cards API.
+        //public List<Card> DealerCards { get; set; } = new List<Card>();
+        //public List<Card> PlayerCards { get; set; } = new List<Card>();
+        public int DealerScore { get; set; }
+        public int PlayerScore { get; set; }
+        public bool GameOver { get { return Outcome != null; } }
+        public string? Outcome { get; set; }
+
+        public GameState(string deckId)
+        {
+            DeckId = deckId;
+        }
+    }
+}
